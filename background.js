383ada3848146a17fd5f84549e7617763bff093e
neptunepink <purpleposeidon@gmail.com>
2017-11-14 01:46:36 -0800

    Firefox had a hard crash in this state.

---
+++ background.js
@@ -0,0 +1,51 @@
+// FIXME: "use strict";
+function openPage() {
+    browser.tabs.query({
+        currentWindow: true,
+        active: true,
+    }).then(tabs => {
+        tabs.forEach(tab => {
+            browser.tabs.sendMessage(tab.id, { toggle: true });
+        });
+    });
+}
+
+function updateBadge(message, sender, reply) {
+    var title = ("Rule: " + message.trash_styles + "\n" +
+                "Style: " + message.trash_elements + "\n" +
+                "CDN: " + message.requires_security_hack);
+    browser.browserAction.setTitle({
+        title,
+        tabId: sender.tab.id,
+    });
+
+    var count = message.trash_styles + message.trash_elements;
+    var icon;
+    var show_count = false;
+    if (count == 0) {
+        icon = "icons/trash-clean.svg";
+    } else if (message.trash_visible) {
+        icon = "icons/trash-shown.svg";
+    } else {
+        icon = "icons/trash-hidden.svg";
+        show_count = true;
+    }
+    browser.browserAction.setIcon({
+        path: {
+            "16": icon,
+            "32": icon,
+            "48": icon,
+            "96": icon,
+        },
+        tabId: sender.tab.id,
+    });
+
+    show_count = false; // I prefer the minimalism. FIXME: Config option.
+    if (show_count) {
+        var n = message.trash_styles + "/" + message.trash_elements;
+        browser.browserAction.setBadgeText({ text: n });
+    }
+}
+
+browser.browserAction.onClicked.addListener(openPage);
+browser.runtime.onMessage.addListener(updateBadge);
--- gtfo.js
+++ gtfo.js
@@ -1,11 +1,43 @@
 // Please forgive my javascript, I am but a humble systems programmer unfamiliar with your j-ways.
-console.log("position:gtfo start");
+// FIXME: "use strict";
 
 // HashSet of css rules & element styles that have had trash stylings.
+// FIXME: Copy the selectors into our own stylesheet?
 var effed_off_styles = new Set();
-// FIXME: Use our own stylesheet instead?
+// If this is true, we must check computedStyles, which can be very slow.
+var requires_security_hack = false;
+var trash_styles = 0;
+var trash_elements = 0;
+var trash_class =  "trash_" + Math.random().toString(36).substring(2);
+var trusted_css = undefined;
+var badge_needs_update = true;
+
+function alert_security_droids(error) {
+    if (!requires_security_hack && error.name == "SecurityError") {
+        requires_security_hack = true;
+        var trusted_style = document.createElement("style");
+        trusted_style.type = "text/css"
+        trusted_style.innerHTML = "." + trash_class + " { display: none; }";
+        document.head.appendChild(trusted_style);
+        trusted_css = trusted_style.sheet;
+        effed_off_styles.add(trusted_css.cssRules[0].style);
+        badge_needs_update = true;
+    }
+}
+
+function set_foul_trash_display(show) {
+    effed_off_styles.forEach(style => {
+        if (show) {
+            style.display = "";
+        } else {
+            style.display = "none";
+        }
+    });
+}
 
 function scan_css_for_trash() {
+    // FIXME: This isn't necessary, right? We'll get to them from document.head?
+    /*
     //var css = window.styleSheets, rules;
     var css = document.styleSheets;
     if (!css) return;
@@ -14,12 +46,13 @@ function scan_css_for_trash() {
         if (typeof css[i] === "object") {
             try {
                 var rules = css[i].rules || css[i].cssRules;
-            } catch (e) {
-                continue;
+            } catch (err) {
+                alert_security_droids(err);
             }
             scan_for_trash(rules);
         }
     }
+    */
 }
 
 function scan_dom_for_trash() {
@@ -27,16 +60,27 @@ function scan_dom_for_trash() {
     scan_element_for_trash(document.body);
 }
 
-function scan_element_for_trash(e) {
-    if (!e) return;
-    scan_for_trash(e.style);
-    if (e.sheet) {
-        var sheet = e.sheet.rules || e.sheet.cssRules;
+function scan_element_for_trash(element) {
+    if (!element) return;
+    scan_for_trash(element.style);
+    if (element.sheet) {
+        try {
+            // wait, wtf, this works? Do the JS guys not know how scopes work?
+            var sheet = element.sheet.rules || element.sheet.cssRules;
+        } catch (err) {
+            alert_security_droids(err);
+        }
         scan_for_trash(sheet);
     }
-    if (!e.children) return;
-    for (var i in e.children) {
-        scan_element_for_trash(e.children[i]);
+    if (requires_security_hack && element.classList && element.classList.length > 0) {
+        var style = window.getComputedStyle(element);
+        if (needs_to_gtfo(style)) {
+            your_element_it_is_trash(element);
+        }
+    }
+    if (!element.children) return;
+    for (var i in element.children) {
+        scan_element_for_trash(element.children[i]);
     }
 }
 
@@ -57,24 +101,35 @@ function scan_for_trash(rules) {
     }
 }
 
+function your_style_it_is_trash(style) {
+    style.display = "none";
+    // style.border = "5px solid cyan";
+    // style.opacity = "0.5";
+}
+
+function your_element_it_is_trash(element) {
+    // function takes care of dupes
+    element.classList.add(trash_class);
+
+    //your_style_it_is_trash(element.style);
+    trash_elements += 1;
+    badge_needs_update = true;
+}
+
 function update_sinners() {
-    console.log("effed_off_styles: ", effed_off_styles);
     effed_off_styles.forEach(function(style) {
-        console.log("Checking sinner ", style);
         if (needs_to_gtfo(style)) {
-            your_style_is_trash(style);
+            your_style_it_is_trash(style);
+            trash_styles += 1;
+            badge_needs_update = true;
         }
     });
 }
 
 
 
-function your_style_is_trash(style) {
-    // style.display = "none";
-    style.border = "5px solid cyan";
-    style.opacity = "0.5";
-}
 
+// https://developer.mozilla.org/en-US/docs/Web/API/MutationRecord
 var body_observer_config = {
     "childList": true,
     "attributes": true,
@@ -85,14 +140,15 @@ var body_observer = new MutationObserver(function(mutations, observer) {
     pause_observers();
     try {
         mutations.forEach(function(mutation) {
-            if (mutation.target.style.position != "fixed") return;
+            if (!needs_to_gtfo(mutation.target.style)) return;
             effed_off_styles.add(mutation.target.style);
-            your_style_is_trash(mutation.target.style);
+            your_style_it_is_trash(mutation.target.style);
         });
     } catch (e) {
-        console.log(e);
+        console.log("body_observer: " + e);
+    } finally {
+        resume_observers();
     }
-    resume_observers();
 });
 
 var stylesheet_observer_config = {
@@ -100,7 +156,6 @@ var stylesheet_observer_config = {
 };
 var stylesheet_observer = new MutationObserver(function(mutations, observer) {
     var any_change = false;
-    console.log("mutation", mutations);
     mutations.forEach(function(mutation) {
         for (var i = 0; i < mutation.addedNodes.length; i++) {
             var node = mutation.addedNodes[i];
@@ -130,33 +185,62 @@ function resume_observers() {
     }
 }
 
+function check_body_scrolling() {
+    if (trash_styles == 0 && trash_elements == 0) return;
+    try {
+        if (document.body.style.overflow == "hidden") {
+            document.body.style.overflow = "visible";
+        }
+    } catch (e) {
+        console.log("check_body_scrolling: " + e);
+    }
+}
+
+function update_background_script() {
+    browser.runtime.sendMessage({
+        trash_styles,
+        trash_elements,
+        requires_security_hack,
+        trash_visible,
+    });
+}
+
 resume_observers();
 function check_all() {
     pause_observers();
     scan_css_for_trash();
     scan_dom_for_trash();
     update_sinners();
+    check_body_scrolling();
     resume_observers();
+    if (badge_needs_update) {
+        badge_needs_update = false;
+        update_background_script();
+    }
 }
+
+// FIXME: Which? Both? Uh?
 window.onload = check_all;
 document.onload = check_all;
-document.onclick = check_all;
 
-// resource://gre/modules/ExtensionContent.jsm -> moz-extension://87324442-0a91-49b5-b84e-b1aecf07adc6/gtfo.js
+var trash_visible = false;
+browser.runtime.onMessage.addListener((message, sender, reply) => {
+    if (message.toggle) {
+        trash_visible = !trash_visible;
+        set_foul_trash_display(trash_visible);
+        update_background_script();
+    }
+    if (message.update) {
+        update_background_script();
+    }
+});
 
 
-
-// Allow position:fixed to appear after anything has been clicked?
-// Some classess should be whitelisted depending on domain?
-// Anything that has position:fixed should perhaps have its node moved to the front? Uh. Like, the issue is if there's a thing at the bottom, but it's an infinite page.
-// Okay, here we go.
-// We add our own nice li'l position:fixed element ;)
-// It's a button. If hovered over, it reveals everything that is position:fixed.
+// Sometimes the user'll want to actually see stuff, eg menu bars.
+// Probably the way to go is to have the trash start out visible, and then fade out.
+// So, CSS animations?
+// And there'll be a toolbar button to toggle them on & off.
 //
-//
-// https://developer.mozilla.org/en-US/docs/Web/API/MutationRecord
 
 // If a user has manually navigated to a webpage using a bookmark or by typing in the URL, they are probably interested enough in the webpage to want to see the stuff.
 // Some pages might emulate position:fixed by using position:absolute & animating it downwards. (position: absolutely not?)
-
-console.log("position:gtfo ran");
